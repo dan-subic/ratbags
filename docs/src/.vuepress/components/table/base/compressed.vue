bfb13a12fb2bdf9aaeddc189e7540da543514988
TARRASQUE\subda <subdamil@hotmail.com>
2024-08-06 19:27:20 +0930

    Compressed table now centres text when horizontal

--- docs/src/.vuepress/components/table/base/compressed.vue
+++ docs/src/.vuepress/components/table/base/compressed.vue
@@ -5,64 +5,69 @@
 -->
 
 <template>
-    <div>
-      <table>
-        <tbody>
-          <tr v-for="row of rows">
-            <td v-for="col of row">
-              {{ col }}
-            </td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
-  </template>
-    
-  <script>
-  import _ from 'lodash'
-  
-  export default {
-    props: {
-      content: {
-        required: true,
-        type: Array,
-      },
-      vertical: {
-        type: Boolean,
-      }
+  <div>
+    <table>
+      <tbody v-bind:class="{ centred: !vertical }">
+        <tr v-for="row of rows">
+          <td v-for="col of row">
+            {{ col }}
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+import _ from 'lodash'
+
+export default {
+  props: {
+    content: {
+      required: true,
+      type: Array,
     },
-    data() {
-      return {
-      }
-    },
-    computed: {
-      rows() {
-        // Generate the content & the die ranges.
-        const content_unique = _.uniq(this.content)
-        const counts = _.countBy(this.content)
+    vertical: {
+      type: Boolean,
+    }
+  },
+  data() {
+    return {
+    }
+  },
+  computed: {
+    rows() {
+      // Generate the content & the die ranges.
+      const content_unique = _.uniq(this.content)
+      const counts = _.countBy(this.content)
 
-        let ranges = _.map(content_unique, (result) => {
-          const first_instance = _.indexOf(this.content, result) + 1
-          let range_str = "" + first_instance
-          if (counts[result] !== 1) {
-            range_str += "-" + (first_instance + counts[result] - 1)
-          }
-          return range_str
-        })
-
-        const table = [
-          ranges,
-          content_unique,
-        ]
-
-        if (!this.vertical) {
-          return table
-        } else {
-          // Transpose
-          return _.map(table[0], (col, colIdx) => _.map(table, (row) => row[colIdx]))
+      let ranges = _.map(content_unique, (result) => {
+        const first_instance = _.indexOf(this.content, result) + 1
+        let range_str = "" + first_instance
+        if (counts[result] !== 1) {
+          range_str += "-" + (first_instance + counts[result] - 1)
         }
-      },
+        return range_str
+      })
+
+      const table = [
+        ranges,
+        content_unique,
+      ]
+
+      if (!this.vertical) {
+        return table
+      } else {
+        // Transpose
+        return _.map(table[0], (col, colIdx) => _.map(table, (row) => row[colIdx]))
+      }
     },
+  },
+}
+</script>
+
+<style lang="css">
+  .centred {
+    text-align: center;
   }
-  </script>
-    
+</style>
