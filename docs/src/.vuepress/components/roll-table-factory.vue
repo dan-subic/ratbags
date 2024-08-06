afdce1ab4daeb00f23e38b3de92c5bd76a8855d7
TARRASQUE\subda <subdamil@hotmail.com>
2024-08-06 11:40:41 +0930

    Table now works. Using successfully in tables page for the names.

---
+++ docs/src/.vuepress/components/roll-table-factory.vue
@@ -0,0 +1,46 @@
+<template>
+  <roll-table :cols="cols" :content="content" />
+</template>
+
+<script>
+import RollTable from "./roll-table.vue";
+import {names} from "../../.data/names.ts";
+
+export default {
+  components: {
+    RollTable,
+  },
+  props: {
+    table: {
+      required: true,
+      type: String,
+    },
+  },
+  data() {
+    return {
+      content: [],
+      cols: 1,
+    }
+  },
+  mounted() {
+    switch(this.table) {
+      case 'female-names': {
+        this.content = names.female
+        this.cols = 4
+        break
+      }
+      case 'male-names': {
+        this.content = names.male
+        this.cols = 4
+        break
+      }
+      case 'surnames': {
+        this.content = names.surnames
+        this.cols = 4
+        break
+      }
+    }
+  },
+}
+</script>
+  
--- docs/src/.vuepress/components/roll-table.vue
+++ docs/src/.vuepress/components/roll-table.vue
@@ -13,37 +13,49 @@
 </template>
   
 <script>
-  export default {
-    props: {
-      cols: {
-        required: true,
-        type: Number,
-      },
-      content: {
-        required: true,
-        type: Array,
-      },
+import _ from 'lodash'
+
+export default {
+  props: {
+    cols: {
+      required: true,
+      type: Number,
     },
-    data() {
-      return {
-        msg: 'Hello this is <demo-component>'
-      }
+    content: {
+      required: true,
+      type: Array,
     },
-    computed: {
-      rows() {
-        const row_num = this.content.length / this.cols
-        let rows = []
+  },
+  data() {
+    return {
+    }
+  },
+  computed: {
+    rows() {
+      // Initialise table matrix.
+      // Has twice as many columns, for the numbers.
+      const row_num = this.content.length / this.cols
+      const single_row = Array(this.cols * 2)
+      const rows_matrix = Array(row_num)
+      // Fill matrix.
+      _.forEach(rows_matrix, (row, idx) => {
+        rows_matrix[idx] = Array.from(single_row)
+      })
+      
+
+      for (let col = 0; col < this.cols; col++) {
         for (let row = 0; row < row_num; row++) {
-          let row_content = []
-          for (let col = 0; col < this.cols; col++) {
-            row_content.push(this.content[(row * this.cols) + col])
-          }
-          rows.push(row_content)
+          // Fill matrix with indices
+          const index = 1 + col + (col * this.cols) + row
+          rows_matrix[row][col * 2] = index
+
+          // Fill matrix with content
+          rows_matrix[row][col * 2 + 1] = this.content[index - 1]
         }
-        // Need to transpose.
-        return rows
-      },
+      }
+      return rows_matrix
     },
-  }
-  </script>
+  },
+}
+</script>
   
